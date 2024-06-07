cedf1ef563fb3863a8e9b9f0379a95f801630557
Cesar <cesar23augusto16@gmail.com>
2024-06-07 16:39:11 -0600

    missing db corrected

--- config.py
+++ config.py
@@ -9,3 +9,5 @@ CORS(app)
 app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 #postgres://database_hackaton_user:z0R8ZoYeu2hkj0bHqbO0Tucb1ZkAWZKI@dpg-cphmn8gcmk4c73ekt5g0-a.oregon-postgres.render.com/database_hackaton
+
+db = SQLAlchemy(app)
